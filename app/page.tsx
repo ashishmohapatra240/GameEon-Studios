1d4b18bae8638cdc389d393d2cb1e87210648885
ashishmohapatra240 <ashishmohapatra240@gmail.com>
2024-09-18 01:59:56 +0530

    Added Our Games Section

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Details from "@/components/NavBar/Details/Details";
 import HeroSection from "@/components/NavBar/HeroSection/HeroSection";
+import OurGames from "@/components/Our Games/OurGames";
 
 export default function Home() {
   return (
     <div>
       <HeroSection/>
       <Details/>
+      <OurGames/>
     </div>
   );
 }
