515a52f7c250f492d5f55c5e4a592a9482a64615
ashishmohapatra240 <ashishmohapatra240@gmail.com>
2024-09-18 02:35:59 +0530

    Improved Responsiveness in TitleSection

--- components/HeroSection/TitleSection.tsx
+++ components/HeroSection/TitleSection.tsx
@@ -1,19 +1,17 @@
 const TitleSection = () => (
-    <div className="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center py-20">
-      <span className="text-white text-6xl font-bold leading-tight">
-        Mumbai Gullies
-      </span>
-      <span className="text-white text-5xl font-bold leading-10">
-        <br />
-      </span>
-      <span className="text-white text-5xl font-normal leading-tight">
-        is exactly the game we have collectively dreamt for
-      </span>
-      <div className="text-white/70 text-base font-regular leading-tight mt-4">
-        A game inspired by the amazing contrast of life of Mumbai City, A city
-        where the landscape changes every few meters.
-      </div>
+  <div className="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center px-4 py-10 sm:py-16 md:py-20">
+    <span className="text-white text-5xl sm:text-5xl md:text-6xl font-bold leading-tight whitespace-nowrap">
+      Mumbai Gullies
+    </span>
+      <br />
+    <span className="text-white text-3xl sm:text-3xl md:text-4xl font-normal leading-tight">
+      is exactly the game <br/>we have collectively dreamt
+    </span>
+    <div className="text-white/70 text-sm sm:text-base md:text-lg font-regular leading-tight mt-4">
+      A game inspired by the amazing contrast of life of Mumbai City, A city
+      where the landscape changes every few meters.
     </div>
-  );
+  </div>
+);
 
-  export default TitleSection;
+export default TitleSection;
