b246d0fd032ef0d6dc9d25dd2b2b9b5673f89012
ashishmohapatra240 <ashishmohapatra240@gmail.com>
2024-09-18 18:55:46 +0530

    Increased Padding in Button Section

--- components/HeroSection/ButtonSection.tsx
+++ components/HeroSection/ButtonSection.tsx
@@ -1,17 +1,12 @@
 const ButtonSection = () => (
-    <div className="absolute left-1/2 top-[70%] transform -translate-x-1/2 flex gap-6">
-      <div className="w-40 h-12 px-9 py-3.5 bg-gradient-to-r from-teal-500 to-teal-500 rounded-lg flex justify-center items-center">
-        <div className="text-white text-base font-semibold">
-          Get started
-        </div>
-      </div>
-      <div className="w-40 h-12 px-9 py-3.5 rounded-lg border border-white flex justify-center items-center">
-        <div className="text-white text-base font-normal">
-          Learn more
-        </div>
-      </div>
+  <div className="absolute left-1/2 top-[70%] transform -translate-x-1/2 flex gap-6 pt-10">
+    <div className="w-40 h-12 px-9 py-3.5 bg-gradient-to-r from-teal-500 to-teal-500 rounded-lg flex justify-center items-center">
+      <div className="text-white text-base font-semibold">Get started</div>
     </div>
-  );
-  
-  export default ButtonSection;
-  
+    <div className="w-40 h-12 px-9 py-3.5 rounded-lg border border-white flex justify-center items-center">
+      <div className="text-white text-base font-normal">Learn more</div>
+    </div>
+  </div>
+);
+
+export default ButtonSection;
