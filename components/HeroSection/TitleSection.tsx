c1e543dff975dacfd42f9393d226886072c99291
ashishmohapatra240 <ashishmohapatra240@gmail.com>
2024-09-18 15:52:11 +0530

    Updated with Mumbai Gullies logo

--- components/HeroSection/TitleSection.tsx
+++ components/HeroSection/TitleSection.tsx
@@ -1,14 +1,19 @@
+import Image from "next/image";
+
 const TitleSection = () => (
-  <div className="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center px-4 py-10 sm:py-16 md:py-20">
-    <span className="text-white text-5xl sm:text-5xl md:text-6xl font-bold leading-tight whitespace-nowrap">
-      Mumbai Gullies
+  <div className="absolute inset-0 flex flex-col items-center justify-center text-center px-4 py-10 sm:py-16 md:py-20">
+    <Image
+      height={200}
+      width={200}
+      src="/images/mg-logo.png"
+      alt="Mumbai Gullies"
+      className="h-auto w-auto"
+    />
+    <span className="text-white text-3xl sm:text-3xl md:text-4xl font-bold leading-tight mt-4">
+      is exactly the game we have<br/>collectively dreamt for
     </span>
-      <br />
-    <span className="text-white text-3xl sm:text-3xl md:text-4xl font-normal leading-tight">
-      is exactly the game <br/>we have collectively dreamt
-    </span>
-    <div className="text-white/70 text-sm sm:text-base md:text-lg font-regular leading-tight mt-4">
-      A game inspired by the amazing contrast of life of Mumbai City, A city
+    <div className="text-white/70 text-sm sm:text-base md:text-lg font-regular leading-tight mt-4 mb-16">
+      A game inspired by the amazing contrast of life of Mumbai City,<br/> A city
       where the landscape changes every few meters.
     </div>
   </div>
